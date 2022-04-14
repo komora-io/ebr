de43eb315e516a1402aa2e0e2af3d124ac6b8ffa
Tyler Neely <t@jujit.su>
2022-04-14 12:17:47 +0200

    Add crossbeam comparison benchmarks

--- benches/defer.rs
+++ benches/defer.rs
@@ -5,9 +5,14 @@ extern crate test;
 use test::Bencher;
 
 use crossbeam_utils::thread::scope;
+use crossbeam_epoch::{Shared, Owned, pin as crossbeam_pin};
+
+const THREADS: usize = 16;
+const STEPS: usize = 10_000;
+
 
 #[bench]
-fn single_alloc_defer_free(b: &mut Bencher) {
+fn ebr_single_alloc_defer_free(b: &mut Bencher) {
     let mut ebr: ebr::Ebr<Box<u64>> = ebr::Ebr::default();
 
     b.iter(|| {
@@ -17,7 +22,7 @@ fn single_alloc_defer_free(b: &mut Bencher) {
 }
 
 #[bench]
-fn single_defer(b: &mut Bencher) {
+fn ebr_single_defer(b: &mut Bencher) {
     let mut ebr: ebr::Ebr<()> = ebr::Ebr::default();
 
     b.iter(|| {
@@ -27,10 +32,7 @@ fn single_defer(b: &mut Bencher) {
 }
 
 #[bench]
-fn multi_alloc_defer_free(b: &mut Bencher) {
-    const THREADS: usize = 16;
-    const STEPS: usize = 10_000;
-
+fn ebr_multi_alloc_defer_free(b: &mut Bencher) {
     b.iter(|| {
         let ebr: ebr::Ebr<Box<u64>> = ebr::Ebr::default();
 
@@ -50,10 +52,7 @@ fn multi_alloc_defer_free(b: &mut Bencher) {
 }
 
 #[bench]
-fn multi_defer(b: &mut Bencher) {
-    const THREADS: usize = 16;
-    const STEPS: usize = 10_000;
-
+fn ebr_multi_defer(b: &mut Bencher) {
     b.iter(|| {
         let ebr: ebr::Ebr<()> = ebr::Ebr::default();
 
@@ -64,6 +63,64 @@ fn multi_defer(b: &mut Bencher) {
                     for _ in 0..STEPS {
                         let mut guard = ebr.pin();
                         guard.defer_drop(());
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+}
+
+#[bench]
+fn crossbeam_single_alloc_defer_free(b: &mut Bencher) {
+    b.iter(|| {
+        let mut guard = crossbeam_pin();
+        unsafe {
+            guard.defer_destroy(Owned::new(1).into_shared(&guard));
+        }
+    });
+}
+
+#[bench]
+fn crossbeam_single_defer(b: &mut Bencher) {
+    b.iter(|| {
+        let mut guard = crossbeam_pin();
+        unsafe {
+            guard.defer_destroy(Owned::new(()).into_shared(&guard));
+        }
+    });
+}
+
+#[bench]
+fn crossbeam_multi_alloc_defer_free(b: &mut Bencher) {
+    b.iter(|| {
+        scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(move |_| {
+                    for _ in 0..STEPS {
+                        let mut guard = crossbeam_pin();
+                        unsafe {
+                            guard.defer_destroy(Owned::new(1).into_shared(&guard));
+                        }
+                    }
+                });
+            }
+        })
+        .unwrap();
+    });
+}
+
+#[bench]
+fn crossbeam_multi_defer(b: &mut Bencher) {
+    b.iter(|| {
+        scope(|s| {
+            for _ in 0..THREADS {
+                s.spawn(move |_| {
+                    for _ in 0..STEPS {
+                        let mut guard = crossbeam_pin();
+                        unsafe {
+                            guard.defer_destroy(Owned::new(()).into_shared(&guard));
+                        }
                     }
                 });
             }
